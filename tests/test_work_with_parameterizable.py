28b118a80804fbc1d049e59617b8b9ce5620c3e3
Vlad Pavlov <vlpavlov@ieee.org>
2025-06-19 01:14:42 -0500

    TST: improve tests for work with parameterizable

--- tests/test_work_with_parameterizable.py
+++ tests/test_work_with_parameterizable.py
@@ -1,11 +1,12 @@
-# from persidict import FileDirDict
+from parameterizable import *
+
 
 def test_file_dir_dict_registration():
-    from parameterizable import is_registered
     from src.persidict import FileDirDict
     assert is_registered(FileDirDict)
+    smoketest_parameterizable_class(FileDirDict)
 
 def test_s3_dict_registration():
-    from parameterizable import is_registered
     from src.persidict import S3Dict
     assert is_registered(S3Dict)
+    smoketest_parameterizable_class(S3Dict)
