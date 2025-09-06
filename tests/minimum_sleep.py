ab8e8e281fb6062a49b7cecf184335f57580b5c6
Vlad Pavlov <vlpavlov@ieee.org>
2025-09-06 18:17:45 -0500

    TST: add unit tests for oldest_keys(), newest_keys(), oldest_values(), and newest_values() functions

---
+++ tests/minimum_sleep.py
@@ -0,0 +1,12 @@
+import time
+
+from src.persidict import FileDirDict, S3Dict
+
+def min_sleep(dct: FileDirDict | S3Dict) -> None:
+    """ Sleep for a minimum time to ensure different timestamps. """
+    if isinstance(dct,FileDirDict):
+        time.sleep(0.17)
+    elif isinstance(dct,S3Dict):
+        time.sleep(1.1)
+    else:
+        raise ValueError(f"Unknown dict type: {type(dct)}")
