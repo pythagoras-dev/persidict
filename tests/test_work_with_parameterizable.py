e75cd137ec42ea206eef608389e3ffed5fbb3fb8
Vlad Pavlov <vlpavlov@ieee.org>
2025-09-14 15:29:54 -0500

    CLN: remove unused tests and add `mock_aws` to S3Dict test

--- tests/test_work_with_parameterizable.py
+++ tests/test_work_with_parameterizable.py
@@ -1,25 +1,13 @@
 from parameterizable import *
-
+from moto import mock_aws
 
 def test_file_dir_dict_registration():
     from persidict import FileDirDict
     assert is_registered(FileDirDict)
     smoketest_parameterizable_class(FileDirDict)
 
-
+@mock_aws
 def test_s3_dict_registration():
     from persidict import S3Dict
     assert is_registered(S3Dict)
     smoketest_parameterizable_class(S3Dict)
-
-
-def test_keep_current_registration():
-    from persidict import KeepCurrentFlag
-    assert is_registered(KeepCurrentFlag)
-    smoketest_parameterizable_class(KeepCurrentFlag)
-
-
-def test_delete_current_registration():
-    from persidict import DeleteCurrentFlag
-    assert is_registered(DeleteCurrentFlag)
-    smoketest_parameterizable_class(DeleteCurrentFlag)
