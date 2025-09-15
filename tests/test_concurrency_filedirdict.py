7cbaf9916722e482bf0bffeab9245a79a0377b49
Vlad Pavlov <vlpavlov@ieee.org>
2025-09-14 20:24:02 -0500

    TST: consolidate tests in test_concurrency_filedirdict.py

--- tests/test_concurrency_filedirdict.py
+++ tests/test_concurrency_filedirdict.py
@@ -19,6 +19,22 @@ def many_operations(base_dir:str, process_n:int):
         except Exception as e:
             d[f"error_in_process_{i}_{e.__class__.__name__}"] = True
 
+def test_concurrency_5(tmpdir):
+    base_dir = str(tmpdir)
+    processes = []
+    for i in range(5):
+        p = multiprocessing.Process(target=many_operations, args=(base_dir,i,))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
+    d = FileDirDict(base_dir=base_dir)
+    assert len(d) == 1, f"Expected 1 item, found {len(d)} items: {list(d.keys())}"
+    assert "a" in d
+    assert isinstance(d["a"], float)
+
+
+
 def test_concurrency_10(tmpdir):
     base_dir = str(tmpdir)
     processes = []
