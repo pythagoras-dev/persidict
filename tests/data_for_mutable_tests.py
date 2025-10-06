68c755beeffb2d711742c85826d4a6f06eb4151c
Vlad Pavlov <vlpavlov@ieee.org>
2025-10-05 19:39:47 -0500

    TST: cleanup tests

--- tests/data_for_mutable_tests.py
+++ tests/data_for_mutable_tests.py
@@ -1,36 +1,28 @@
 from persidict import FileDirDict, BasicS3Dict, LocalDict, S3Dict_FileDirCached
-# from persidict.s3_dict_legacy import S3Dict_Legacy
 
 
 mutable_tests = [
 
 (FileDirDict, dict(serialization_format="pkl", digest_len=11))
 ,(FileDirDict, dict(serialization_format="json", digest_len=11))
-# ,(S3Dict_Legacy, dict(serialization_format="pkl", bucket_name="my_bucket", digest_len=11))
-# ,(S3Dict_Legacy, dict(serialization_format="json", bucket_name="her_bucket", digest_len=11))
 ,(S3Dict_FileDirCached, dict(serialization_format="pkl", bucket_name="my_bucket", digest_len=11))
 ,(S3Dict_FileDirCached, dict(serialization_format="json", bucket_name="her_bucket", digest_len=11))
 
 
-
 ,(FileDirDict, dict(serialization_format="pkl", digest_len=5))
 ,(FileDirDict, dict(serialization_format="json", digest_len=5))
-# ,(S3Dict_Legacy, dict(serialization_format="pkl", bucket_name="my_bucket", digest_len=5))
-# ,(S3Dict_Legacy, dict(serialization_format="json", bucket_name="his_bucket", digest_len=5))
 ,(S3Dict_FileDirCached, dict(serialization_format="pkl", bucket_name="my_bucket", digest_len=5))
 ,(S3Dict_FileDirCached, dict(serialization_format="json", bucket_name="his_bucket", digest_len=5))
 
+
 ,(FileDirDict, dict(serialization_format="pkl", digest_len=0))
 ,(FileDirDict, dict(serialization_format="json", digest_len=0))
-# ,(S3Dict_Legacy, dict(serialization_format="pkl", bucket_name="my_bucket", digest_len=0))
-# ,(S3Dict_Legacy, dict(serialization_format="json", bucket_name="her_bucket", digest_len=0))
 ,(S3Dict_FileDirCached, dict(serialization_format="pkl", bucket_name="my_bucket", digest_len=0))
 ,(S3Dict_FileDirCached, dict(serialization_format="json", bucket_name="her_bucket", digest_len=0))
 
+
 ,(FileDirDict, dict(serialization_format="pkl"))
 ,(FileDirDict, dict(serialization_format="json"))
-# ,(S3Dict_Legacy, dict(serialization_format="pkl", bucket_name="her_bucket"))
-# ,(S3Dict_Legacy, dict(serialization_format="json", bucket_name="their_bucket"))
 ,(S3Dict_FileDirCached, dict(serialization_format="pkl", bucket_name="her_bucket"))
 ,(S3Dict_FileDirCached, dict(serialization_format="json", bucket_name="their_bucket"))
 ,(BasicS3Dict, dict(serialization_format="pkl", bucket_name="super_bucket"))
@@ -38,8 +30,7 @@ mutable_tests = [
 ,(LocalDict, dict(serialization_format="json", bucket_name="first_bucket"))
 ,(LocalDict, dict(serialization_format="pkl", bucket_name="second_bucket"))
 
-# ,(S3Dict_Legacy, dict(serialization_format="pkl", bucket_name="a_bucket", root_prefix ="_"))
-# ,(S3Dict_Legacy, dict(serialization_format="json", bucket_name="the_bucket", root_prefix ="OYO"))
+
 ,(S3Dict_FileDirCached, dict(serialization_format="pkl", bucket_name="a_bucket", root_prefix ="_"))
 ,(S3Dict_FileDirCached, dict(serialization_format="json", bucket_name="the_bucket", root_prefix ="OYO"))
 ,(BasicS3Dict, dict(serialization_format="pkl", bucket_name="a_bucket", root_prefix = "_"))
--- tests/test_work_with_pandas.py
+++ tests/test_work_with_pandas.py
@@ -19,17 +19,17 @@ def test_work_with_pandas(tmpdir, DictToTest, kwargs):
     model_dict["z"] = pd.DataFrame({"a": [1, 2, 3, 4, 5]})
     assert (dict_to_test["z"] == model_dict["z"]).sum().sum() == 5
 
-    # dict_to_test["zz"] = pd.Series([1, 2, 3, 4, 5, 6])
-    # model_dict["zz"] = pd.Series([1, 2, 3, 4, 5, 6])
-    # assert (dict_to_test["zz"] == model_dict["zz"]).sum() == 6
-    #
-    # dict_to_test["zzz"] = pd.Index([1, 2, 3, 4])
-    # model_dict["zzz"] = pd.Index([1, 2, 3, 4])
-    # assert (dict_to_test["zzz"] == model_dict["zzz"]).sum() == 4
-    #
-    # dict_to_test["zzzz"] = pd.RangeIndex(0, 15)
-    # model_dict["zzzz"] = pd.RangeIndex(0, 15)
-    # assert (dict_to_test["zzzz"] == model_dict["zzzz"]).sum() == 15
+    dict_to_test["zz"] = pd.Series([1, 2, 3, 4, 5, 6])
+    model_dict["zz"] = pd.Series([1, 2, 3, 4, 5, 6])
+    assert (dict_to_test["zz"] == model_dict["zz"]).sum() == 6
+
+    dict_to_test["zzz"] = pd.Index([1, 2, 3, 4])
+    model_dict["zzz"] = pd.Index([1, 2, 3, 4])
+    assert (dict_to_test["zzz"] == model_dict["zzz"]).sum() == 4
+
+    dict_to_test["zzzz"] = pd.RangeIndex(0, 15)
+    model_dict["zzzz"] = pd.RangeIndex(0, 15)
+    assert (dict_to_test["zzzz"] == model_dict["zzzz"]).sum() == 15
 
     #TODO: add MultiIndex tests
 
