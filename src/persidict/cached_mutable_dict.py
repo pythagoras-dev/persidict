2d032e4661f552f4fb94d878e779c5beb9549680
Vlad Pavlov <vlpavlov@ieee.org>
2026-02-11 13:27:22 +0300

    REF: Remove unused `KEEP_CURRENT` import in `cached_mutable_dict.py`

--- src/persidict/cached_mutable_dict.py
+++ src/persidict/cached_mutable_dict.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from .persi_dict import PersiDict, NonEmptyPersiDictKey, PersiDictKey, ValueType
 from .safe_str_tuple import NonEmptySafeStrTuple, SafeStrTuple
 from .jokers_and_status_flags import (EXECUTION_IS_COMPLETE,
-                                      KEEP_CURRENT, DELETE_CURRENT,
+                                      DELETE_CURRENT,
                                       ETagValue,
                                       ETagConditionFlag,
                                       ETAG_HAS_CHANGED,
